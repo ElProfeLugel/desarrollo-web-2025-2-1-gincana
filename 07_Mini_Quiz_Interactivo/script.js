c6bbf4518079b5c7b4a202cdb4e099b8e58aa3a7
B14s207 <aagudelo483@soyudemedellin.edu.co>
2025-08-13 20:15:57 -0500

    Aaron- first commit

---
+++ 07_Mini_Quiz_Interactivo/script.js
@@ -0,0 +1,17 @@
+class Datos{
+    constructor(pregunta, opciones, respuestaCorrecta){
+        this.pregunta = pregunta
+        this.opciones = opciones
+        this.respuestaCorrecta
+    }
+
+    
+}
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const datos=[]
+    datos.push(["¿Cuáles son los colores primarios?", ["A. Amarillo, Azul, Rojo","B. Morado, rosado, gris", "C.Verde, Azul, Rojo"], "A. Amarillo, Azul, Rojo"])
+    let pregunta= document.getElementById("Pregunta")
+})
+
