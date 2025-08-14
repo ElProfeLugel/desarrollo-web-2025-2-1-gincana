736cf313923b10a8859cf0125b935f8ac414376a
Lugel <Lugel>
2025-08-13 19:36:12 -0500

    Laura Bedoya - "Correción de variable y colores"

--- 05_Reproductor_Musica/java.js
+++ 05_Reproductor_Musica/java.js
@@ -1,26 +1,26 @@
 
   const audio = document.getElementById("audioPlayer");
   const playPauseBtn = document.getElementById("BotonPausa");
-  const stopBtn = document.getElementById("stopBtn");
-  const progressBar = document.getElementById("progressBar");
-  let isPlaying = false;
+  const stopBtn = document.getElementById("BotonStop");
+  const barraProgreso = document.getElementById("barraProgreso");
+  let estasonando = false;
 
   playPauseBtn.addEventListener("click", () => {
-    if (isPlaying) {
+    if (estasonando) {
       audio.pause();
       playPauseBtn.textContent = "Play";
     } else {
       audio.play();
       playPauseBtn.textContent = "Pause";
     }
-    isPlaying = !isPlaying;
+    estasonando = !estasonando;
   });
 
   stopBtn.addEventListener("click", () => {
     audio.pause();
     audio.currentTime = 0;
     playPauseBtn.textContent = "Play";
-    isPlaying = false;
+    estasonando = false;
     updateProgressBar();
   });
 
@@ -30,5 +30,5 @@
 
   function updateProgressBar() {
     const progress = (audio.currentTime / audio.duration) * 100;
-    progressBar.style.width = progress + "%";
+    barraProgreso.style.width = progress + "%";
   }
