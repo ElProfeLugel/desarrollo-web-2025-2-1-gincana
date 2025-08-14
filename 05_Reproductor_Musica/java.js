f9c2199dda20325a3d6557a536a6045e2d88d59f
Lugel <Lugel>
2025-08-13 19:24:42 -0500

    Laura Bedoya-"Separación de código en sus archivos"

--- 05_Reproductor_Musica/java.js
+++ 05_Reproductor_Musica/java.js
@@ -1,33 +1,34 @@
-const audio = document.getElementById("audioPlayer");
-const playPauseBtn = document.getElementById("playPauseBtn");
-const stopBtn = document.getElementById("stopBtn");
-const progressBar = document.getElementById("progressBar");
-let isPlaying = false;
 
-playPauseBtn.addEventListener("click", () => {
-  if (isPlaying) {
+  const audio = document.getElementById("audioPlayer");
+  const playPauseBtn = document.getElementById("BotonPausa");
+  const stopBtn = document.getElementById("stopBtn");
+  const progressBar = document.getElementById("progressBar");
+  let isPlaying = false;
+
+  playPauseBtn.addEventListener("click", () => {
+    if (isPlaying) {
+      audio.pause();
+      playPauseBtn.textContent = "Play";
+    } else {
+      audio.play();
+      playPauseBtn.textContent = "Pause";
+    }
+    isPlaying = !isPlaying;
+  });
+
+  stopBtn.addEventListener("click", () => {
     audio.pause();
+    audio.currentTime = 0;
     playPauseBtn.textContent = "Play";
-  } else {
-    audio.play();
-    playPauseBtn.textContent = "Pause";
+    isPlaying = false;
+    updateProgressBar();
+  });
+
+  audio.addEventListener("timeupdate", () => {
+    updateProgressBar();
+  });
+
+  function updateProgressBar() {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.style.width = progress + "%";
   }
-  isPlaying = !isPlaying;
-});
-
-stopBtn.addEventListener("click", () => {
-  audio.pause();
-  audio.currentTime = 0;
-  playPauseBtn.textContent = "Play";
-  isPlaying = false;
-  updateProgressBar();
-});
-
-audio.addEventListener("timeupdate", () => {
-  updateProgressBar();
-});
-
-function updateProgressBar() {
-  const progress = (audio.currentTime / audio.duration) * 100;
-  progressBar.style.width = progress + "%";
-}
