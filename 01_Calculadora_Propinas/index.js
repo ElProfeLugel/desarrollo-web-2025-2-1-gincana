40415c1adafb900a1b1fbd261a045e28a3ea156f
lugel <lugel>
2025-08-13 19:37:02 -0500

    dariana_lopera_y_brahian_osorio

--- 01_Calculadora_Propinas/index.js
+++ 01_Calculadora_Propinas/index.js
@@ -1,8 +1,41 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    let porcentajePropina = Array.from(querySelectorAll("button"))
+
+    let botones = Array.from(document.querySelectorAll("button"));
+
+    botones[0].addEventListener('click', calcularPropina5);
+
+    console.log(botones);
 
 
-    function calcularPropina(totalCuenta, cantidaPersonas, porcentajePropina){
+
+    function calcularPropina5(){
+        let totalCuenta = document.querySelector("#total");
+        let cantidadPersonas = document.querySelector("#cantidad");
+
+        let resultado = (totalCuenta + 0.05) / cantidadPersonas;
+
+        const label =document.querySelector("#resultado");
+        label.innerText = resultado
+
+
+    }
+
+    function calcularPropina10(){
+        let totalCuenta = document.querySelector("#total");
+        let cantidadPersonas = document.querySelector("#cantidad");
+
+        return (totalCuenta + 0.10) / cantidadPersonas
+
+
+    }
+
+    function calcularPropina15(){
+
+        let totalCuenta = document.querySelector("#total");
+        let cantidadPersonas = document.querySelector("#cantidad");
+
+        return (totalCuenta + 0.15) / cantidadPersonas
+
 
     }
 })
