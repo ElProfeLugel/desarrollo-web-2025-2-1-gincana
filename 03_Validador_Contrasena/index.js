bd3ac55ac5138e75a2619b96ea47cf3773a5629f
Mariaisabel2 <mquinteroisabel32@gmail.com>
2025-08-13 19:37:57 -0500

    Isabella Ceballos Bedoya -  Realice la primera verificacion.( se verifica que sea mayor de 8 caracteres)

--- 03_Validador_Contrasena/index.js
+++ 03_Validador_Contrasena/index.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    const contraseña=document.getElementById("password").addEventListener("input",Contraseña())
 
 
+    function Check(){
+    const password = contraseña.value
 
-
+/* se verifica que sean minimo 8 caracteres */
+const minimo8caracteres = document.getElementById("minimo8caracteres");
+if (password.length >= 8){  
+    minimo8caracteres.classList.add("valid");
+    minimo8caracteres.classList.remove("invalid");   
+}else { 
+    minimo8caracteres.classList.add("invalid");
+    minimo8caracteres.classList.remove("valid");
+}
 
 })
