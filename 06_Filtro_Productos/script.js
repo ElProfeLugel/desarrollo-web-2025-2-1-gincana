f088949a56bd70f47b6b54b609a02122abe8cb0a
Valuv <valuv027@gmail.com>
2025-08-13 19:12:01 -0500

    Josué-"Se inicia el proceso de filtros"

---
+++ 06_Filtro_Productos/script.js
@@ -0,0 +1,7 @@
+//Esta funcion nos ayudará a filtrar los elementos
+
+function filterObjects(filter) {
+    var i, elements;
+    elements = document.getElementsByClassName("cars");
+    if (filter == "all")
+}
